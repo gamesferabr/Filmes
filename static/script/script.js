61d129feb55c406ad96e2db2e67a8d67825e7674
gamesferabr <gabriel@timepix.com.br>
2023-12-01 12:36:54 -0800

    modified:   static/script/script.js

--- static/script/script.js
+++ static/script/script.js
@@ -4,15 +4,14 @@ window.onload = function() {
         let data = await response.json();
         let articlemovies = document.getElementsByTagName("article")
         
-        console.log(data);
 
         for(let i = 0; i < articlemovies.length; i++){
             const nomefilme = document.querySelector(`.nomefilme${i}`);
             nomefilme.textContent = data.movies[i].title; 
 
             const ratingfilme = document.querySelector(`.score${i}`);
-            ratingfilme.textContent = data.movies[i].vote_average;
-
+            ratingfilme.textContent = data.movies[i].vote_average.toFixed(1);
+            
             const imagemfilme = document.querySelector(`.img${i}`);
             imagemfilme.src = "https://image.tmdb.org/t/p/w500/" + data.movies[i].poster_path;
             imagemfilme.alt = data.movies[i].title;
